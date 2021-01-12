6cfffdaa7303b6ccffee49db1f836875833e842d
Sakhawat-hossain <mdsakhawathossain05@gmail.com>
2021-01-13 00:27:54 +0600

    stack implementation where push/pop/min all in O(1)

--- StackWithMinMax.java
+++ StackWithMinMax.java
@@ -18,6 +18,16 @@ public class StackWithMinMax {
 				else if(m==3) System.out.println(sm.getMax());
 			}
 		}
+		else if(flag==2){
+			StackMin sm = new StackMin();
+			for(int i=0;i<n; i++){
+				m = sc.nextInt();
+				if(m==1) {
+					v = sc.nextInt();sm.push(v);}
+				else if(m==2) System.out.println(sm.pop());
+				else if(m==3) System.out.println(sm.getMin());
+			}
+		}
 	}
 }
 class StackMax{
@@ -63,3 +73,47 @@ class StackMax{
 	}
 }
 //"stack implementation where push/pop/max all in O(1)"
+
+class StackMin{
+	private Stack<Integer>  stack; 
+	private int min; // used to store min value
+	
+	public StackMin(){
+		stack = new Stack<>();
+	}
+	// modifying push method
+	public void push(int n){
+		if(stack.isEmpty()){
+			min = n;
+			stack.push(n);
+			return;
+		}	
+		//when n < min
+		if(n<min){
+			stack.push(2*n - min);
+			min = n;
+		}
+		else
+			stack.push(n);
+	}
+	// modifying pop method
+	public Integer pop(){
+		if(stack.isEmpty()){
+			System.out.println("The stack is empty");
+			return null;
+		}	
+		//when n < min
+		int n = stack.pop();
+		if(n<min){
+			int t = min;
+			min = 2*min - n;
+			n = t;
+		}
+		//System.out.println(n);
+		return n;
+	}	
+	public int getMin(){
+		return min;
+	}
+}
+//"stack implementation where push/pop/min all in O(1)"
