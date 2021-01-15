f85dd81cb93b559a95adbcbcd5a442cb45ca5215
Sakhawat-hossain <mdsakhawathossain05@gmail.com>
2021-01-15 17:34:52 +0600

    some general algo added

--- GeneralAlgo.java
+++ GeneralAlgo.java
@@ -1,15 +1,23 @@
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Map;
 
+//file path --- F:\Job material\Algorithm
 public class GeneralAlgo {
 	public static void main(String []args){
 		System.out.println("General algorithms:");
 		SomeAlgo sa = new SomeAlgo();
+		System.out.println("\nSequence divided by 3:");
 		sa.printSeqDiv(100);
+		System.out.println("\nPrint pellindrome:");
 		sa.printSeqN(10);
 		int arr[] = {1, 2, 3, 4, 5, 1, 3, 4};
+		System.out.println("\nRemove/print duplicate:");
 		sa.printDuplicate(arr);
+
+		System.out.println("\nprint bit representation:");
+		sa.printBit(-2);
 	}
 }
 	
@@ -47,6 +55,9 @@ class SomeAlgo{
 			System.out.print(integer + " ");
 		}
 		System.out.println();
+		for (Map.Entry<Integer, Integer> e : hm.entrySet()) {
+			System.out.println(e.getKey() + " -> " + e.getValue());
+		}
 		
 		System.out.println( hs.toString() + "  Length: " + hs.size());
 		for (Integer integer : hs) {
@@ -56,7 +67,32 @@ class SomeAlgo{
 		Iterator<Integer> itr = hs.iterator();
 		while(itr.hasNext())
 			System.out.print(itr.next() + " ");
-		hs.forEach(System.out::println);
+		hs.forEach(System.out::print);
 	}
-	
+
+	public void printBit(int n){
+		int nn = n;
+		int t = 1 << 31;
+		for(int i=1; i<=32; i++){
+			if((n & t) == t)
+				System.out.print("1");
+			else
+				System.out.print("0");
+			n <<= 1;
+			if(i%8 == 0)
+				System.out.print(" ");
+		}
+		System.out.println(" ");
+		n = nn;
+		for(int i=1; i<=32; i++){
+			if((n & 1) == 1)
+				System.out.print("1");
+			else
+				System.out.print("0");
+			n >>= 1;
+			if(i%8 == 0)
+				System.out.print(" ");
+		}
+		System.out.println(" ");
+	}
 }
